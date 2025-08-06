de1c018b59a6645724fafb117d3560722ae0f759
teamkitkumazaki <41033533+teamkitkumazaki@users.noreply.github.com>
2025-08-06 23:16:29 +0900

    404ページの作成

--- _wp/404.php
+++ _wp/404.php
@@ -4,25 +4,23 @@
 <?php get_template_part("parts/head");?>
 </head>
 <body>
-<header>
+<header class="underpage">
 <?php get_template_part("parts/header");?>
 </header>
 <article id="page404" class="page-404">
-	<section class="section-error">
-		<div class="section_error_inner">
-			<h1 class="page_ttl">404 NOT FOUND</h1>
-			<div class="error_description">
-				<p>お探しのページは見つかりません。</p>
-				<p>お探しのページは一時的にアクセスできない状況にあるか、移動もしくは削除された可能性があります。</p>
-				<p>また、URL、ファイル名にタイプミスがないか再度ご確認ください。</p>
+	<section class="section-404">
+		<div class="section_inner">
+			<h1 class="title404">404 NOT FOUND</h1>
+			<div class="description_404">
+				<h2 class="sub_ttl_404">ページが見つかりませんでした</h2>
+				<p class="desc_404">お探しのページは存在しないようです。<br>メニューより目的のページをお探しください</p>
 			</div>
-			<div class="comp-button-wrapper">
-				<div class="button_item">
-					<a href="/">ホームへ戻る</a>
-				</div>
+			<div class="back_button">
+				<a href="/">トップページに戻る</a>
 			</div>
-		</div>
+		</div><!-- section_inner -->
 	</section>
+	<?php get_template_part("parts/recruit");?>
 </article>
 <?php get_template_part("parts/hummenu");?>
 <?php get_template_part("parts/footer");?>
